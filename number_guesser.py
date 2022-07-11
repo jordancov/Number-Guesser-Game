31b9f0ec41fdb2fa9747cccd3f66606bb9a02bbd
Jordan Coverdale <jordancover2@gmail.com>
2022-07-11 17:51:58 +0100

    first commit

---
+++ number_guesser.py
@@ -0,0 +1,23 @@
+import random
+
+numberOneRange = 0
+numberTwoRange = 20
+correctAnswer = random.randrange(numberOneRange, numberTwoRange)
+print(correctAnswer)
+
+print("Hello! Please enter your name")
+name = input("Enter name: ")
+
+print("Welcome", name, "this is the number guesser game")
+print("Please guess a number between", numberOneRange, "and", numberTwoRange)
+
+userAnswer = input("Enter number: ")
+
+while userAnswer != correctAnswer:
+    print("WRONG ANSWER TRY AGAIN") 
+    userAnswer = input("Enter number: ")
+    if userAnswer == correctAnswer:
+        print("WOW well done you got the right number!")
+        break
+
+    
