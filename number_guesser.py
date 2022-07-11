266d27604faab06190d0102531488c61f4ad8f77
Jordan Coverdale <jordancover2@gmail.com>
2022-07-11 18:15:05 +0100

    Added comments

--- number_guesser.py
+++ number_guesser.py
@@ -1,18 +1,20 @@
+#importing packages
 import random
 
+#Defining random numbers
 numberOneRange = 0
 numberTwoRange = 20
 correctAnswer = random.randrange(numberOneRange, numberTwoRange)
 print(correctAnswer)
 
+#Intial input from user
 print("Hello! Please enter your name")
 name = input("Enter name: ")
-
 print("Welcome", name, "this is the number guesser game")
 print("Please guess a number between", numberOneRange, "and", numberTwoRange)
-
 userAnswer = input("Enter number: ")
 
+#While statement to capture the answer from the user
 while(True):
     if userAnswer != correctAnswer:
         print("WRONG ANSWER TRY AGAIN") 
